6d6830672d91742e5091a58d26d0d8efbb839cde
AsadbekAzimov <asadbekazimov400@gmail.com>
2022-10-30 11:33:58 +0500

    first commi

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ let num1 = myRandom(1, 100)
 let num2 = myRandom(1, 100)
 let val = `${num1} ${users[randomAction]} ${num2}`
 
-let pr = +prompt(`${num1} ${users[randomAction]} ${num2}`)
+let pr = +prompt(`${val}`)
 
 
 let answer = pr == `${eval(val)}` ? `Javob to'g'ri` : `javobingiz noto'g'ri. To'g'ri javob: ${eval(val)}`
